92c69f919e4624491db9aade2fc9b03c97d9d638
anlinpanda <anlin_c@yahoo.ca>
2020-05-11 12:25:00 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.224479bf2fc2432851879ed989486d27.js"
+  "/precache-manifest.15f2ca18ccdc2717e2fc0be599ea6909.js"
 );
 
 self.addEventListener('message', (event) => {
